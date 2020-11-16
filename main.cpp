98179a058c5648ed43a551bf5fa0c5aba76317a7
jttmtv <52265704+jttmtv@users.noreply.github.com>
2020-11-16 16:30:20 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include "matrix.hpp"
+#include "matrix.h"
 #include <chrono>
 #include <iostream>
 #define ROW_SIZE 1000
@@ -17,32 +17,61 @@ int main()
     matrix_set(&A);
     matrix_set(&B);
 
-    // for (int i = 0; i < 8; i++)
-    //     for (int j = 0; j < 8; j++)
-    //         B.val[i][j] = j;
-    // for (int i = 0; i < 8; i++)
-    //     for (int j = 0; j < 8; j++)
-    //         A.val[i][j] = j;
-    if (check(&A, &B))
-    {
-        matrix_clear(&C);
-        matrix_transpose(&B, &BT);
-        auto start = std::chrono::steady_clock::now();
-        matrix_multiplication5(&A, &BT, &C);
-        auto end = std::chrono::steady_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-        std::cout << "total times have been used:" << duration << "ms" << std::endl;
-    }
-    else
-        std::cerr << "ILLEGAL DATA" << std::endl;
+    std::cout<< "1: Brute force\n"
+        << "2: Change the order of the loop\n"
+        << "3: Blocked Matrix\n"
+        << "4: Preliminary AVX instruction set\n"
+        << "5: UNROOL + AVX\n"
+        << "6: OpenMp + AVX\n"
+        << std::endl;
+        if (check(&A, &B))
+        {
+            matrix_clear(&C);
+            auto start = std::chrono::steady_clock::now();
+            matrix_multiplication1(&A, &B, &C);
+            auto end = std::chrono::steady_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+            std::cout << "Solution1 total times have been used:" << duration << "ms" << std::endl;
 
-    // for (int i = 0; i < 8; i++)
-    // {
-    //     for (int j = 0; j < 8; j++)
-    //         std::cout << C.val[i][j] << " ";
-    //     std::cout << std::endl;
-    // }
+            matrix_clear(&C);
+            start = std::chrono::steady_clock::now();
+            matrix_multiplication2(&A, &B, &C);
+            end = std::chrono::steady_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+            std::cout << "Solution2 total times have been used:" << duration << "ms" << std::endl;
 
+            matrix_clear(&C);
+            start = std::chrono::steady_clock::now();
+            matrix_multiplication3(&A, &B, &C);
+            end = std::chrono::steady_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+            std::cout << "Solution3 total times have been used:" << duration << "ms" << std::endl;
+
+            matrix_clear(&C);
+            start = std::chrono::steady_clock::now();
+            matrix_multiplication4(&A, &B, &C);
+            end = std::chrono::steady_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+            std::cout << "Solution4 total times have been used:" << duration << "ms" << std::endl;
+
+            matrix_clear(&C);
+            matrix_transpose(&B, &BT);
+            start = std::chrono::steady_clock::now();
+            matrix_multiplication5(&A, &BT, &C);
+            end = std::chrono::steady_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+            std::cout << "Solution5 total times have been used:" << duration << "ms" << std::endl;
+
+            matrix_clear(&C);
+            matrix_transpose(&B, &BT);
+            start = std::chrono::steady_clock::now();
+            matrix_multiplication5(&A, &BT, &C);
+            end = std::chrono::steady_clock::now();
+            duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+            std::cout << "Solution6 total times have been used:" << duration << "ms" << std::endl;
+        }
+        else
+            std::cerr << "ILLEGAL DATA" << std::endl;
     memory_free(&A);
     memory_free(&B);
     memory_free(&C);
