41d0dc922c4ab6d01acfd46a7979ee40883a15d9
jttmtv <52265704+jttmtv@users.noreply.github.com>
2020-11-26 22:00:26 +0800

    Update matrix.hpp

--- matrix.hpp
+++ matrix.hpp
@@ -15,6 +15,7 @@ void matrix_clear(matrix*);
 void matrix_transpose(const matrix*, matrix*);
 void memory_access(matrix*, size_t, size_t);
 void memory_free(matrix*);
+void block(const matrix *A, const matrix *B, matrix *C, size_t, size_t, size_t);
 
 void matrix_multiplication1(const matrix*, const matrix*, matrix*);
 void matrix_multiplication2(const matrix*, const matrix*, matrix*);
