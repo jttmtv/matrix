819e4c9ecc7a7336687d5015607a9a38bbe7ba76
jttmtv <52265704+jttmtv@users.noreply.github.com>
2020-12-20 12:53:07 +0800

    Update fast_sgemm.cpp

--- fast_sgemm.cpp
+++ fast_sgemm.cpp
@@ -73,9 +73,9 @@ void core_mu(const float* matA, const float* matB, float* matC, const size_t M, 
 {
 	try
 	{
-		float* packMatA = new float[M * K];
-		float* dst_unitA = packMatA;		
-		for (size_t i = 0; i < M; i += 8) {
+		float* packA = new float[M * K];
+		float* dst_unitA = packA;
+		for (size_t i = 0; i < M - M % 8; i += 8) {
 			for (size_t k = 0; k < K; ++k) {
 				const float* src_unitA = &matA[i * K + k];
 				const __m256 src_dataA = _mm256_set_ps(src_unitA[7 * K], src_unitA[6 * K], src_unitA[5 * K], src_unitA[4 * K], src_unitA[3 * K], src_unitA[2 * K], src_unitA[1 * K], src_unitA[0 * K]);
@@ -83,9 +83,9 @@ void core_mu(const float* matA, const float* matB, float* matC, const size_t M, 
 				dst_unitA += 8;
 			}
 		}
-		float* packMatB = new float[K * N];
-		float* dst_unitB = packMatB;
-		for (size_t j = 0; j < N; j += 8) {
+		float* packB = new float[K * N];
+		float* dst_unitB = packB;
+		for (size_t j = 0; j < N - N % 8; j += 8) {
 			for (size_t k = 0; k < K; ++k) {
 				_mm256_storeu_ps(dst_unitB, _mm256_loadu_ps(&matB[k * N + j]));
 				dst_unitB += 8;
@@ -95,11 +95,11 @@ void core_mu(const float* matA, const float* matB, float* matC, const size_t M, 
 		omp_set_num_threads(NUM_THREADS);
 #pragma omp parallel for 
 #endif
-		for (size_t j = 0; j < N; j += 8)
-			for (size_t i = 0; i < M; i += 8)
-				core_cb(&packMatA[i * K], &packMatB[j * K], &matC[i * N + j], M, K, N);
-		delete[] packMatA;
-		delete[] packMatB;
+		for (size_t j = 0; j < N - N % 8; j += 8)
+			for (size_t i = 0; i < M - M % 8; i += 8)
+				core_cb(&packA[i * K], &packB[j * K], &matC[i * N + j], M, K, N);
+		delete[] packA;
+		delete[] packB;
 	}
 	catch (bad_alloc& ME) {
 		cerr << ME.what() << endl;
@@ -108,30 +108,24 @@ void core_mu(const float* matA, const float* matB, float* matC, const size_t M, 
 
 void core_ma(const float* matA, const float* matB, float* matC, const size_t M, const size_t K, const size_t N)
 {
-	try {
-		const size_t M8 = (M / 8 + 1) * 8;
-		const size_t K8 = (K / 8 + 1) * 8;
-		const size_t N8 = (N / 8 + 1) * 8;
-		float* cloA = new float[M8 * K8];
-		float* cloB = new float[K8 * N8];
-		float* cloC = new float[M8 * N8];
-		memset(cloA, 0.0f, sizeof(float) * M8 * K8);
-		memset(cloB, 0.0f, sizeof(float) * K8 * N8);
-		memset(cloC, 0.0f, sizeof(float) * M8 * N8);
-		for (size_t i = 0; i < M; ++i)
-			memcpy(cloA + i * K8, matA + i * K, sizeof(float) * K);
-		for (size_t i = 0; i < K; ++i)
-			memcpy(cloB + i * N8, matB + i * N, sizeof(float) * N);
-		core_mu(cloA, cloB, cloC, M8, K8, N8);
-		for (size_t i = 0; i < M; ++i)
-			memcpy(matC + i * N, cloC + i * N8, sizeof(float) * N);
-		delete[] cloA;
-		delete[] cloB;
-		delete[] cloC;
-	}
-	catch (bad_alloc& ME) {
-		cerr << ME.what() << endl;
-	}
+	const size_t M8 = M - M % 8, N8 = N - N % 8;
+	core_mu(matA, matB, matC, M, K, N);
+#ifdef _OPENMP
+	omp_set_num_threads(NUM_THREADS);
+#pragma omp parallel for 
+#endif
+	for (size_t m = M8; m < M; ++m)
+		for (size_t k = 0; k < K; ++k)
+			for (size_t n = 0; n < N; ++n)
+				matC[m * N + n] += matA[m * K + k] * matB[k * N + n];
+#ifdef _OPENMP
+	omp_set_num_threads(NUM_THREADS);
+#pragma omp parallel for 
+#endif
+	for (size_t m = 0; m < M8; ++m)
+		for (size_t k = 0; k < K; ++k)
+			for (size_t n = N8; n < N; ++n)
+				matC[m * N + n] += matA[m * K + k] * matB[k * N + n];
 }
 
 void core_sm(const float* matA, const float* matB, float* matC, const size_t M, const size_t K, const size_t N)
@@ -151,9 +145,9 @@ bool fast_sgemm(const float* A, const float* B, float* C, const size_t M, const 
 	if (!(M && K && N))
 		return false;
 	memset(C, 0.0f, sizeof(float) * M * N);
-	if (!(M % 8 || N % 8 || K % 8))
+	if (!(M % 8 || N % 8))
 		core_mu(A, B, C, M, K, N);
-	else if (M < 100 && K < 100 && N < 100)
+	else if (M < 8 && N < 8)
 		core_sm(A, B, C, M, K, N);
 	else
 		core_ma(A, B, C, M, K, N);
@@ -162,9 +156,9 @@ bool fast_sgemm(const float* A, const float* B, float* C, const size_t M, const 
 
 int main()
 {
-	const size_t M = 999;
-	const size_t K = 999;
-	const size_t N = 999;
+	const size_t M = 9999;
+	const size_t K = 9999;
+	const size_t N = 9999;
 	float* A = new float[M * K];
 	float* B = new float[K * N];
 	float* C = new float[M * N];
@@ -183,12 +177,12 @@ int main()
 	auto duration = duration_cast<microseconds>(end - start);
 	cout << double(duration.count()) * microseconds::period::num / microseconds::period::den << "s" << endl;
 	core_sm(A, B, CC, M, K, N);
-	for (size_t i = 0; i < M*N; i++)
+	for (size_t i = 0; i < M * N; i++)
 	{
 		if (abs(C[i] - CC[i]) > 1e-6f)
 			cout << i << endl;
 	}
-	//cout << "OK" << endl;
+	cout << "OK" << endl;
 	//start = system_clock::now();
 	//fast_sgemm(A, B, C, M, K, N);
 	//end = system_clock::now();
